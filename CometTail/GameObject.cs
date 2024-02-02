fd437a10a27d83ac75dfbf2be3dea7602212813e
Ghoomba <81963750+Ghoomba@users.noreply.github.com>
2024-02-02 01:30:47 -0500

    Created CometTail project and added the GameObject, Comet, and Obstacle classes

---
+++ CometTail/GameObject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CometTail
+{
+    internal abstract class GameObject
+    {
+        // Fields
+        protected Texture2D texture;
+        protected Rectangle position;
+
+        // Properties
+        /// <summary>
+        /// Read-only property for texture
+        /// </summary>
+        public Texture2D Texture 
+        { 
+            get { return texture; } 
+        }
+
+        /// <summary>
+        /// Read-only property for position
+        /// </summary>
+        public Rectangle Position
+        {
+            get { return position; }
+        }
+
+        // Constructors
+        /// <summary>
+        /// Initializes fields of the game object class
+        /// </summary>
+        /// <param name="texture">Texture of game object</param>
+        /// <param name="position">Position of game object</param>
+        public GameObject(Texture2D texture, Rectangle position) 
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        // Methods
+        /// <summary>
+        /// Update frames
+        /// </summary>
+        /// <param name="gameTime">Game Time</param>
+        public abstract void Update(GameTime gameTime);
+
+        /// <summary>
+        /// Draw GameObject to the window
+        /// </summary>
+        /// <param name="sb">Sprite Batch</param>
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(
+                texture,
+                position,
+                Color.White);
+        }
+    }
+}
---
+++ CometTail/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new CometTail.Game1();
+game.Run();
